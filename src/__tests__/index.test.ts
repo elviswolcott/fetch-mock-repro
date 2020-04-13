6c98526f0b994adf9c9b2a97ff1deb2213b7e9f1
Elvis <elviswolcott@gmail.com>
2020-04-12 20:12:41 -0500

    test: SDK should return an image and answer

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,7 +1,60 @@
-import { hello } from "../index";
+import { yesno } from "../index";
 
-describe("hello world", () => {
-  it('should return "Hello world."', () => {
-    expect(hello()).toBe("Hello world.");
+jest.mock("node-fetch", () => {
+  const nodeFetch = jest.requireActual("node-fetch");
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const mock = require("fetch-mock");
+  const mocked = mock.sandbox();
+  /*
+  Object.assign(mocked.config, {
+    fetch: nodeFetch,
+  });
+  */
+  const { default: exclude, ...internals } = nodeFetch;
+  return {
+    default: mocked,
+    __esModule: true,
+    ...internals,
+    ...mocked,
+  };
+});
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const fetchMock = require("node-fetch");
+import fetch, { Request, Response } from "node-fetch";
+
+afterEach(() => {
+  fetchMock.reset();
+});
+
+// JSON to Request body
+const jsonBody = (json: object): ArrayBuffer => {
+  const content = JSON.stringify(json, null, 2);
+  return Uint8Array.from(Buffer.from(content)).buffer;
+};
+
+const no = (url: string, options: object, request: Request): Response =>
+  new Response(
+    jsonBody({
+      // ensures ticket depends on credentials
+      answer: "no",
+      image:
+        "https://yesno.wtf/assets/no/26-34b31d1f0777f70c61488f67a36576a9.gif",
+      forced: false,
+    }),
+    {
+      status: 200,
+    }
+  );
+
+describe("YesNo SDK", () => {
+  it('answers "yes" or "no"', async () => {
+    fetchMock.mock("https://yesno.wtf/api", no);
+    const answer = (await yesno()).answer;
+    expect(answer).toMatch(/(yes|no)/);
+  });
+  it("includes an image", async () => {
+    fetchMock.mock("https://yesno.wtf/api", no);
+    const image = (await yesno()).image;
+    expect(image).toBeTruthy();
   });
 });
