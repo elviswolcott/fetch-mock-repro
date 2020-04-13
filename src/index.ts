ab2c0d7f680b48a1b1a94b8fc6de8a0aa1888585
Elvis <elviswolcott@gmail.com>
2020-04-12 20:11:27 -0500

    feat: impliment YesNo SDK

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
-export const hello = (): string => {
-  return "Hello world.";
+import fetch from "node-fetch";
+
+const yesno = async (): Promise<{
+  answer: string;
+  image: string;
+  forced: boolean;
+}> => {
+  const response = await fetch("https://yesno.wtf/api");
+  const json = await response.json();
+  return json;
 };
+
+export { yesno };
